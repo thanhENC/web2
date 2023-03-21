750eedd510a2e263267cb4384224b72804ae7217
Thanh <thanhenc@gmail.com>
2023-03-21 16:50:16 +0700

    conf

--- baitapweb/src/app/app-routing.module.ts
+++ baitapweb/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { Bai29ExternalApiNganHangDongAComponent } from './bai29-external-api-ngan-hang-dong-a/bai29-external-api-ngan-hang-dong-a.component';
 import { Bai30FakeProductComponent } from './bai30-fake-product/bai30-fake-product.component';
 import { Bai31FakeProduct2Component } from './bai31-fake-product2/bai31-fake-product2.component';
+import { Bai32CoindeskAPIComponent } from './bai32-coindesk-api/bai32-coindesk-api.component';
 
 const routes: Routes = [
   { path: 'bai 29', component: Bai29ExternalApiNganHangDongAComponent},
   { path: 'bai 30', component: Bai30FakeProductComponent},
-  { path: 'bai 31', component: Bai31FakeProduct2Component}
+  { path: 'bai 31', component: Bai31FakeProduct2Component},
+  { path: 'bai 32', component: Bai32CoindeskAPIComponent}
 ];
 
 @NgModule({
@@ -21,5 +23,6 @@ export class AppRoutingModule {
 export const RoutingComponent = [
   Bai29ExternalApiNganHangDongAComponent,
   Bai30FakeProductComponent,
-  Bai31FakeProduct2Component
+  Bai31FakeProduct2Component,
+  Bai32CoindeskAPIComponent
 ]
--- baitapweb/src/app/app.module.ts
+++ baitapweb/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { Bai29ExternalApiNganHangDongAComponent } from './bai29-external-api-ngan-hang-dong-a/bai29-external-api-ngan-hang-dong-a.component';
 import { Bai30FakeProductComponent } from './bai30-fake-product/bai30-fake-product.component';
 import { Bai31FakeProduct2Component } from './bai31-fake-product2/bai31-fake-product2.component';
+import { Bai32CoindeskAPIComponent } from './bai32-coindesk-api/bai32-coindesk-api.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { Bai31FakeProduct2Component } from './bai31-fake-product2/bai31-fake-pro
     RoutingComponent,
     Bai29ExternalApiNganHangDongAComponent,
     Bai30FakeProductComponent,
-    Bai31FakeProduct2Component
+    Bai31FakeProduct2Component,
+    Bai32CoindeskAPIComponent
   ],
   imports: [
     BrowserModule,
