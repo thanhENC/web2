a277f592f097e9360f9caa976d50cbbf06dc45d9
Thanh <thanhenc@gmail.com>
2023-03-22 23:21:30 +0700

    mdf

--- angularuel/src/app/interfaces/idong-abank-data.ts
+++
@@ -1,5 +0,0 @@
-import { IDongABankItem } from "./idong-abank-item";
-
-export interface IDongABankData {
-  items: Array<IDongABankItem>
-}
--- angularuel/src/app/interfaces/idong-abank-item.ts
+++
@@ -1,8 +0,0 @@
-export interface IDongABankItem {
-  type: string;
-  imageurl: string;
-  muatienmat: string;
-  muack: string;
-  bantienmat: string;
-  banck: string;
-}
