0cbd78d8cf02284719335179dcaf3e65a392843a
Thanh <thanhenc@gmail.com>
2023-03-23 16:23:29 +0700

    mongodb-server

--- my-server-mongodb/index.js
+++ my-server-mongodb/index.js
@@ -3,11 +3,19 @@ const app = express();
 const port = 3002;
 const morgan = require("morgan")
 app.use(morgan("combined"))
+
+// payload limit 10mb
 const bodyParser = require("body-parser")
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb' }));
+app.use(express.json());
+
 const cors = require("cors");
 app.use(cors())
+
 app.listen(port, () => {
     console.log(`My Server listening on port ${port}`)
 })
@@ -21,7 +29,55 @@ client.connect();
 database = client.db("FashionData");
 fashionCollection = database.collection("Fashion");
 
+// get all fashions
 app.get("/fashions", cors(), async (req, res) => {
     const result = await fashionCollection.find({}).toArray();
     res.send(result)
+})
+
+// get fashion by id
+app.get("/fashions/:id", cors(), async (req, res) => {
+    var o_id = new ObjectId(req.params['id']);
+    const result = await fashionCollection.find({ _id: o_id }).toArray();
+    res.send(result[0])
+})
+
+// post fashion
+app.post("/fashions", cors(), async (req, res) => {
+    // put json Fashion into database
+    await fashionCollection.insertOne(req.body)
+    // send message to client (send all database to client)
+    res.send(req.body)
+})
+
+// put fashion
+app.put("/fashions", cors(), async (req, res) => {
+    //update json Fashion into database
+    await fashionCollection.updateOne(
+        { _id: new ObjectId(req.body._id) },//condition for update
+        {
+            $set: { //Field for updating
+                style: req.body.style,
+                fashion_subject: req.body.fashion_subject,
+                fashion_detail: req.body.fashion_detail,
+                fashion_image: req.body.fashion_image
+            }
+        })
+    //send Fahsion after updating
+    var o_id = new ObjectId(req.body._id);
+    const result = await fashionCollection.find({ _id: o_id }).toArray();
+    res.send(result[0])
+})
+
+// delete fashion
+app.delete("/fashions/:id", cors(), async (req, res) => {
+    //find detail Fashion with id
+    var o_id = new ObjectId(req.params["id"]);
+    const result = await fashionCollection.find({ _id: o_id }).toArray();
+    //update json Fashion into database
+    await fashionCollection.deleteOne(
+        { _id: o_id }
+    )
+    //send Fahsion after remove
+    res.send(result[0])
 })
