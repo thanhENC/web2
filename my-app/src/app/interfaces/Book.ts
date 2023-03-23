085efbd63435dfbb9d9ffedfc105bda53bcb84e8
Thanh <thanhenc@gmail.com>
2023-03-23 09:47:56 +0700

    book

--- my-app/src/app/interfaces/Book.ts
+++ my-app/src/app/interfaces/Book.ts
@@ -4,3 +4,12 @@ export interface IBook {
   Price: number;
   Image: string;
 }
+
+export class Book{
+  constructor(
+    public BookId: string='',
+    public BookName: string='',
+    public Price: number=0,
+    public Image: string=''
+  ){}
+}
--- my-app/src/app/services/book-api.service.ts
+++ my-app/src/app/services/book-api.service.ts
@@ -10,6 +10,7 @@ export class BookAPIService {
 
   constructor(private _http: HttpClient) { }
 
+  // get all books
   getBooks(): Observable<any>{
     const headers = new HttpHeaders().set(
       'Content-Type',
@@ -28,6 +29,45 @@ export class BookAPIService {
     )
   }
 
+  // get book by id - bai 45
+  getBook(bookId:string): Observable<any>{
+    const headers = new HttpHeaders().set(
+      'Content-Type',
+      'text/plain;charset=utf-8'
+    )
+
+    const requestOptions: Object = {
+      headers: headers,
+      responseType: 'text',
+    }
+
+    return this._http.get<any>("/books/"+bookId, requestOptions).pipe(
+      map((res) => JSON.parse(res) as IBook),
+      retry(3),
+      catchError(this.handleError)
+    )
+  }
+
+  // post book - bai 47
+  postBook(aBook:any): Observable<any>{
+    const headers = new HttpHeaders().set(
+      'Content-Type',
+      'application/json;charset=utf-8'
+    )
+
+    const requestOptions: Object = {
+      headers: headers,
+      responseType: 'text',
+    }
+
+    return this._http.post<any>("/books", JSON.stringify(aBook), requestOptions).pipe(
+      map((res) => JSON.parse(res) as Array<IBook>),
+      retry(3),
+      catchError(this.handleError)
+    )
+  }
+
+  // handle error
   handleError(error: HttpErrorResponse) {
     return throwError(() => new Error(error.message))
   }
