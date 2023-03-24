ed15e08cab48729d7497195f935d43f97ab27cf1
Thanh <thanhenc@gmail.com>
2023-03-24 08:19:14 +0700

    conf

--- baitapweb/src/app/app-routing.module.ts
+++ baitapweb/src/app/app-routing.module.ts
@@ -5,13 +5,19 @@ import { Bai30FakeProductComponent } from './bai30-fake-product/bai30-fake-produ
 import { Bai31FakeProduct2Component } from './bai31-fake-product2/bai31-fake-product2.component';
 import { Bai32CoindeskAPIComponent } from './bai32-coindesk-api/bai32-coindesk-api.component';
 import { Bai33ListPublicAPIsComponent } from './bai33-list-public-apis/bai33-list-public-apis.component';
+import { Bai34PredictGenderComponent } from './bai34-predict-gender/bai34-predict-gender.component';
+import { Bai35UsDataComponent } from './bai35-us-data/bai35-us-data.component';
+import { Bai36RandomDogImageComponent } from './bai36-random-dog-image/bai36-random-dog-image.component';
 
 const routes: Routes = [
   { path: 'bai 29', component: Bai29ExternalApiNganHangDongAComponent},
   { path: 'bai 30', component: Bai30FakeProductComponent},
   { path: 'bai 31', component: Bai31FakeProduct2Component},
   { path: 'bai 32', component: Bai32CoindeskAPIComponent},
-  { path: 'bai 33', component: Bai33ListPublicAPIsComponent}
+  { path: 'bai 33', component: Bai33ListPublicAPIsComponent},
+  { path: 'bai 34', component: Bai34PredictGenderComponent},
+  { path: 'bai 35', component: Bai35UsDataComponent},
+  { path: 'bai 36', component: Bai36RandomDogImageComponent}
 ];
 
 @NgModule({
@@ -27,5 +33,8 @@ export const RoutingComponent = [
   Bai30FakeProductComponent,
   Bai31FakeProduct2Component,
   Bai32CoindeskAPIComponent,
-  Bai33ListPublicAPIsComponent
+  Bai33ListPublicAPIsComponent,
+  Bai34PredictGenderComponent,
+  Bai35UsDataComponent,
+  Bai36RandomDogImageComponent
 ]
--- baitapweb/src/app/app.module.ts
+++ baitapweb/src/app/app.module.ts
@@ -10,6 +10,9 @@ import { Bai30FakeProductComponent } from './bai30-fake-product/bai30-fake-produ
 import { Bai31FakeProduct2Component } from './bai31-fake-product2/bai31-fake-product2.component';
 import { Bai32CoindeskAPIComponent } from './bai32-coindesk-api/bai32-coindesk-api.component';
 import { Bai33ListPublicAPIsComponent } from './bai33-list-public-apis/bai33-list-public-apis.component';
+import { Bai34PredictGenderComponent } from './bai34-predict-gender/bai34-predict-gender.component';
+import { Bai35UsDataComponent } from './bai35-us-data/bai35-us-data.component';
+import { Bai36RandomDogImageComponent } from './bai36-random-dog-image/bai36-random-dog-image.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +22,10 @@ import { Bai33ListPublicAPIsComponent } from './bai33-list-public-apis/bai33-lis
     Bai30FakeProductComponent,
     Bai31FakeProduct2Component,
     Bai32CoindeskAPIComponent,
-    Bai33ListPublicAPIsComponent
+    Bai33ListPublicAPIsComponent,
+    Bai34PredictGenderComponent,
+    Bai35UsDataComponent,
+    Bai36RandomDogImageComponent
   ],
   imports: [
     BrowserModule,
