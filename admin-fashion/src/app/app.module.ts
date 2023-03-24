6a0ec9ee5760ab27ef094510a23ac1203e58bdc6
Thanh <thanhenc@gmail.com>
2023-03-24 19:52:05 +0700

    Bai 62 admin-fashion

---
+++ admin-fashion/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule, RoutingComponents } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FashionComponent } from './fashion/fashion.component';
+import { FashionDetailComponent } from './fashion-detail/fashion-detail.component';
+import { FashionNewComponent } from './fashion-new/fashion-new.component';
+import { FashionUpdateComponent } from './fashion-update/fashion-update.component';
+import { FashionDeleteComponent } from './fashion-delete/fashion-delete.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CKEditorModule } from 'ckeditor4-angular';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FashionComponent,
+    RoutingComponents,
+    FashionDetailComponent,
+    FashionNewComponent,
+    FashionUpdateComponent,
+    FashionDeleteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    CKEditorModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
